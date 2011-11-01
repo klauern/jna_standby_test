c978ac67b99659412da9f81223a78cfd8b1e3256
Nick Klauer <klauer@gmail.com>
2011-10-31 21:57:52 -0500

    formatting

--- src/main/java/klauer/jna/HelloWorld.java
+++ src/main/java/klauer/jna/HelloWorld.java
@@ -10,18 +10,18 @@ import com.sun.jna.Platform;
  */
 public class HelloWorld {
 
-	public interface CLibrary extends Library {
-		CLibrary INSTANCE = (CLibrary) Native.loadLibrary((Platform.isWindows() ? "msvcrt" : "c"),
-						CLibrary.class);
+  public interface CLibrary extends Library {
+    CLibrary INSTANCE = (CLibrary) Native.loadLibrary((Platform.isWindows() ? "msvcrt" : "c"),
+        CLibrary.class);
 
-		void printf(String format, Object... args);
-	}
+    void printf(String format, Object... args);
+  }
 
-	public static void main(String[] args) {
-		CLibrary.INSTANCE.printf("Hello World!");
+  public static void main(String[] args) {
+    CLibrary.INSTANCE.printf("Hello World!");
 
-		for (int i = 0; i < args.length; i++) {
-			CLibrary.INSTANCE.printf("Argument %d: %s\n", i, args[i]);
-		}
-	}
+    for (int i = 0; i < args.length; i++) {
+      CLibrary.INSTANCE.printf("Argument %d: %s\n", i, args[i]);
+    }
+  }
 }
--- src/main/java/klauer/jna/StandBy.java
+++ src/main/java/klauer/jna/StandBy.java
@@ -10,30 +10,30 @@ import com.sun.jna.Native;
  * @author A03182
  */
 public class StandBy {
-	// Basically this follows JNA's how-to, but for the PowrProf library
-	// Windows API is a good reference for these things:
-	// http://msdn.microsoft.com/en-us/library/cc433218(VS.85).aspx
-	public interface PowrProf extends Library {
+  // Basically this follows JNA's how-to, but for the PowrProf library
+  // Windows API is a good reference for these things:
+  // http://msdn.microsoft.com/en-us/library/cc433218(VS.85).aspx
+  public interface PowrProf extends Library {
 
-		// PowrProf is where you can send a laptop into StandbyMode
-		PowrProf INSTANCE = (PowrProf)
-						Native.loadLibrary("powrprof", PowrProf.class);
+    // PowrProf is where you can send a laptop into StandbyMode
+    PowrProf INSTANCE = (PowrProf)
+      Native.loadLibrary("powrprof", PowrProf.class);
 
 
-		// See the MSDN documentation: http://msdn.microsoft.com/en-us/library/aa373201(VS.85).aspx
-		// There are a number of possible methods that can be used, but I only
-		// need to create interface methods for the ones I want to use.
-		boolean SetSuspendState(
-						boolean Hibernate,
-						boolean ForceCritical,
-						boolean DisableWakeEvent);
-	}
+    // See the MSDN documentation: http://msdn.microsoft.com/en-us/library/aa373201(VS.85).aspx
+    // There are a number of possible methods that can be used, but I only
+    // need to create interface methods for the ones I want to use.
+    boolean SetSuspendState(
+        boolean Hibernate,
+        boolean ForceCritical,
+        boolean DisableWakeEvent);
+  }
 
-	public static void main(String[] args) {
-		// Folder for where the powrprof.dll is.
-		System.setProperty("jna.library.path", "C:\\WINDOWS\\system32");
+  public static void main(String[] args) {
+    // Folder for where the powrprof.dll is.
+    System.setProperty("jna.library.path", "C:\\WINDOWS\\system32");
 
-		PowrProf lib = PowrProf.INSTANCE;
-		lib.SetSuspendState(true, false, true);
-	}
+    PowrProf lib = PowrProf.INSTANCE;
+    lib.SetSuspendState(true, false, true);
+  }
 }
